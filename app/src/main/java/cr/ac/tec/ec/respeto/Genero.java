ed971afc70c38db6f29d1738cac83ce710fd9173
Daniel <dab18@hotmail.com>
2018-09-13 17:30:29 -0600

    some updates, classes

---
+++ app/src/main/java/cr/ac/tec/ec/respeto/Genero.java
@@ -0,0 +1,5 @@
+package cr.ac.tec.ec.respeto;
+
+public enum Genero {
+    MASCULINO,FEMENINO;
+}
---
+++ app/src/main/java/cr/ac/tec/ec/respeto/Usuario.java
@@ -0,0 +1,69 @@
+package cr.ac.tec.ec.respeto;
+
+public class Usuario {
+    private int id;
+    private int cedula;
+    private String nombreCompleto;
+    private Genero genero;
+    private int edad;
+    private String alias;
+
+    public Usuario() {}
+
+    public Usuario(int id, int cedula, String nombreCompleto, Genero genero, int edad, String alias) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombreCompleto = nombreCompleto;
+        this.genero = genero;
+        this.edad = edad;
+        this.alias = alias;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
