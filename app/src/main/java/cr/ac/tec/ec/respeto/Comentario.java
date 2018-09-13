c17fd1bf2f2f176e1b66b96879ccee200c4e1a84
Daniel <dab18@hotmail.com>
2018-09-13 17:43:13 -0600

    Comentario added

--- app/src/main/java/cr/ac/tec/ec/respeto/Comentario.java
+++ app/src/main/java/cr/ac/tec/ec/respeto/Comentario.java
@@ -1,4 +1,58 @@
 package cr.ac.tec.ec.respeto;
 
+import java.util.Date;
+
 public class Comentario {
+    private int id;
+    private String cuerpo;
+    private int idUsuario;
+    private Date fechaHora;
+
+
+
+    public Comentario() {
+    }
+
+    public Comentario(int id, String cuerpo, int idUsuario, Date fechaHora) {
+        this.id = id;
+        this.cuerpo = cuerpo;
+        this.idUsuario = idUsuario;
+        this.fechaHora = fechaHora;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+
+
+
 }
